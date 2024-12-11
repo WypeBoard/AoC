2aabde80c3549e9bf8874829b39a7b090e1710de
Hoejholt <nikolajhoejholt@gmail.com>
2024-12-11 22:57:39 +0100

    Added day03 no logic

---
+++ src/main/java/org/example/y2024/Day03.java
@@ -0,0 +1,20 @@
+package org.example.y2024;
+
+import org.example.framework.Day;
+
+public class Day03 extends Day {
+    @Override
+    protected void parseInput() {
+        super.parseInput();
+    }
+
+    @Override
+    public String evalPart1() {
+        return null;
+    }
+
+    @Override
+    public String evalPart2() {
+        return null;
+    }
+}
---
+++ src/test/java/org/example/y2024/Day03Test.java
@@ -0,0 +1,27 @@
+package org.example.y2024;
+
+import org.example.framework.Day;
+
+import java.util.List;
+
+public class Day03Test implements BaseTest {
+    @Override
+    public Day getDay() {
+        return new Day03();
+    }
+
+    @Override
+    public List<String> getPart1TestData() {
+        return List.of();
+    }
+
+    @Override
+    public String getPart1ExpectedResult() {
+        return "";
+    }
+
+    @Override
+    public String getPart2ExpectedResult() {
+        return "";
+    }
+}
